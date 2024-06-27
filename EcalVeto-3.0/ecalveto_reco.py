05d0f54e5ad48c6ff2cea467b6c9bf7f8606dfe4
Danyi Zhang <danyizhang@ucsb.edu>
2024-06-27 16:33:52 -0700

    Add default maxEvent

--- EcalVeto-3.0/ecalveto_reco.py
+++ EcalVeto-3.0/ecalveto_reco.py
@@ -5,7 +5,7 @@ import os
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-i','--infile', action='store', dest='infile', help='Directory of input file, absolute path')
 parser.add_argument('-o','--outdir', action='store', dest='outdir', help='Directory of output file, absolute path')
-parser.add_argument('-m','--maxEvent', type=int, action='store', dest='maxEvent', help='Maximum number of events to process')
+parser.add_argument('-m','--maxEvent', type=int, action='store', default=100000000, dest='maxEvent', help='Maximum number of events to process')
 parser.add_argument('-g','--groupl', action='store', dest='group_label', help='Sample label')
 args = parser.parse_args()
 
